a9d19ec313495124b10ddc075ff96271572004c4
Roman <ayoz4@yandex.ru>
2019-04-26 12:13:58 +0700

    sm

--- src/containers/MainLayoutContainer/containers/Header.jsx
+++ src/containers/MainLayoutContainer/containers/Header.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "../styled/Header.css";
 
-const Header = () => <div className="header">ShopName</div>;
+const Header = () => (
+  <div>
+    <div className="header">ShopName</div>
+    <button>Sign in</button>
+  </div>
+);
 
 export default Header;
